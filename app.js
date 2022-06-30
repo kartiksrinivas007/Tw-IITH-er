53a937cb04046c8d323fc4991757fce3cc64dfa1
Pranav <es20btech11035@iith.ac.in>
2022-06-30 08:10:14 +0530

    added routing for login page

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express  = require('express')
+const express  = require('express');
+const path = require('path');
 const app = express();
 const port = 3003;
-const middleware = require("./middleware")
+const middleware = require("./middleware");
 
-const server = app.listen(port, ()=> console.log("Server listening on port"));
+const server = app.listen(port, ()=> console.log('Server listening on port', port));
 
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, 'public')));
 //when you need a view  go to folder called views
+
+const loginRoute = require('./routes/loginRoutes');
+app.use('/login', loginRoute);
+
 app.get("/", middleware.requireLogin, (req, res, next) => {
-    //Middleware to Create authentication! 
+    //Middleware to Create authentication!
 
     var payload = {
         pageTitle : "Home"
@@ -19,4 +26,4 @@ app.get("/", middleware.requireLogin, (req, res, next) => {
     res.status(200).render("home", payload); //show that status code
 
 })
-//in case you enter something you can't get anything 
+//in case you enter something you can't get anything
