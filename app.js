0c32a66025768fa5cde7c560e0e29eec4c0b5404
Pranav <es20btech11035@iith.ac.in>
2022-06-30 10:52:14 +0530

    finished registration page, added basic client and server side verification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3003;
 const middleware = require("./middleware");
+const bodyParser = require('body-parser');
 
 const server = app.listen(port, ()=> console.log('Server listening on port', port));
 
@@ -10,11 +11,15 @@ const server = app.listen(port, ()=> console.log('Server listening on port', por
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 //when you need a view  go to folder called views
 
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute  = require('./routes/registerRoutes');
+
 app.use('/login', loginRoute);
+app.use('/register', registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     //Middleware to Create authentication!
