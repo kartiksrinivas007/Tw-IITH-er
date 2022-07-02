91e883d07270aaaae9322df25b0163d06411c5cf
Rohith-India <103924801+Rohith-India@users.noreply.github.com>
2022-07-02 22:36:04 +0530

    Add files via upload
    Changing MongoDB reference from localhost to MongoDB cluster.

--- database.js
+++ database.js
@@ -1,14 +1,4 @@
-//The require(‘mongoose’) call above returns a Singleton object. 
-//It means that the first time you call require(‘mongoose’), it 
-//is creating an instance of the Mongoose class and returning it. 
-//On subsequent calls, it will return the same instance that was 
-//created and returned to you the first time because of how module 
-//import/export works in ES6.
 const mongoose = require("mongoose");
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
 
 class Database {
 
@@ -17,12 +7,13 @@ class Database {
     }
 
     connect() {
-        mongoose.connect("mongodb+srv://admin:dbUserPassword@twitterclonecluster.e0mcw.azure.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority")
+        //mongoose.connect('mongodb://localhost:27017/TwitterClone')
+        mongoose.connect("mongodb+srv://admin:mongodbpassword@twitterclonecluster.jyzjns2.mongodb.net/?retryWrites=true&w=majority")
         .then(() => {
-            console.log("database connection successful");
+            console.log("Connected to TwitterClone database...");
         })
         .catch((err) => {
-            console.log("database connection error " + err);
+            console.log("Error while connecting to TwitterClone database: " + err);
         })
     }
 }
