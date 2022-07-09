2976fb6f444e53d0ea23036fa08bf81eee736906
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-07-09 23:19:41 +0530

    Working on reply - Modal

--- public/js/common.js
+++ public/js/common.js
@@ -1,35 +1,243 @@
-//this file contains behaviours that are for all the pages
-// $(document).ready(()=> {
-//     alert("This file is Linked Properly")
+// //this file contains behaviours that are for all the pages
+// // $(document).ready(()=> {
+// //     alert("This file is Linked Properly")
+// // })
+// //small a  for area !!!!
+// $("#postTextarea,#replyTextarea").keyup((event) => {
+//     var textbox = $(event.target);
+//     // console.log(textbox);
+//     var value = textbox.val().trim();
+//     // console.log(value);
+//     var isModal = textbox.parents(".modal").length == 1;
+//     //check if its a reply or a post !
+
+
+//     var submitButton = isModal ? $("#submitReplyButton") : $("submitPostButton");
+
+//     if(submitButton.length == 0) return alert("No submit button found!");
+
+//     if(value == ""){
+//         submitButton.prop("disabled", true);
+//         return;
+//     }
+
+//     submitButton.prop("disabled", false);
 // })
-//small a  for area !!!!
-$("#postTextarea").keyup((event) => {
+// $("#submitPostButton").click(() => {
+//     var button = $(event.target);
+//     var textbox =$("#postTextarea");
+
+//     var data = {
+//         content: textbox.val(),
+//     }
+
+//     $.post("/api/posts", data, postData => {
+
+//         var html = createPostHtml(postData);
+//         $(".postsContainer").prepend(html);
+//         textbox.val("");
+//         button.prop("disabled", true);
+//     })
+// })
+
+// //fire event when modal is opened 
+
+// $("#replyModal").on("show.bs.modal", (event) => {
+//     console.log("Bello"); 
+//     var button = $(event.target);
+//     var postId = getPostIdFromElement(button);
+// })
+// $(document).on("click", ".likeButton", (event) => {
+//     var button = $(event.target);
+//     var postId = getPostIdFromElement(button);
+
+//     if(postId === undefined) return;
+
+//     $.ajax({
+//         url: `/api/posts/${postId}/like`,
+//         type: "PUT",
+//         success: (postData) => {
+//             button.find("span").text(postData.likes.length || "");
+
+//             if(postData.likes.includes(userLoggedIn._id)){
+//                 button.addClass("active");
+//             }
+//             else{
+//                 button.removeClass("active");
+//             }
+//         }
+//     })
+
+// })
+
+// $(document).on("click", ".retweetButton", (event) => {
+//     var button = $(event.target);
+//     var postId = getPostIdFromElement(button);
+    
+//     if(postId === undefined) return;
+
+//     $.ajax({
+//         url: `/api/posts/${postId}/retweet`,
+//         type: "POST",
+//         success: (postData) => {            
+//             button.find("span").text(postData.retweetUsers.length || "");
+
+//             if(postData.retweetUsers.includes(userLoggedIn._id)) {
+//                 button.addClass("active");
+//             }
+//             else {
+//                 button.removeClass("active");
+//             }
+
+//         }
+//     })
+
+// })
+
+// function getPostIdFromElement(element) {
+//     var isRoot = element.hasClass("post");
+//     var rootElement = isRoot == true ? element : element.closest(".post");
+//     var postId = rootElement.data().id;
+
+//     if(postId === undefined) return alert("Post Id undefined");
+
+//     return postId;
+// }
+
+// function createPostHtml(postData) {
+
+//     if(postData == null) return alert("post object is null");
+
+//     var isRetweet = postData.retweetData !== undefined;
+//     var retweetedBy = isRetweet ? postData.postedBy.username : null;
+//     postData = isRetweet ? postData.retweetData : postData;
+
+//     console.log(isRetweet);
+    
+//     var postedBy = postData.postedBy;
+
+//     if(postedBy._id === undefined) {
+//         return console.log("User object not populated");
+//     }
+
+//     var displayName = postedBy.firstName + " " + postedBy.lastName;
+//     var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
+//     var likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? "active" : "";
+//     var retweetButtonActiveClass = postData.retweetUsers.includes(userLoggedIn._id) ? "active" : "";
+
+//     var retweetText = '';
+//     if(isRetweet) {
+//         retweetText = `<span>
+//                         <i class='fas fa-retweet'></i>
+//                         Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a>    
+//                     </span>`
+//     }
+
+//     return `<div class='post' data-id='${postData._id}'>
+//                 <div class='postActionContainer'>
+//                     ${retweetText}
+//                 </div>
+//                 <div class='mainContentContainer'>
+//                     <div class='userImageContainer'>
+//                         <img src='${postedBy.profilePic}'>
+//                     </div>
+//                     <div class='postContentContainer'>
+//                         <div class='header'>
+//                             <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+//                             <span class='username'>@${postedBy.username}</span>
+//                             <span class='date'>${timestamp}</span>
+//                         </div>
+//                         <div class='postBody'>
+//                             <span>${postData.content}</span>
+//                         </div>
+//                         <div class='postFooter'>
+//                             <div class='postButtonContainer'>
+//                                 <button data-toggle='modal' data-target ='#replyModal'>
+//                                     <i class='far fa-comment'></i>
+//                                 </button>
+//                             </div>
+//                             <div class='postButtonContainer green'>
+//                                 <button class='retweetButton ${retweetButtonActiveClass}'>
+//                                     <i class='fas fa-retweet'></i>
+//                                     <span>${postData.retweetUsers.length || ""}</span>
+//                                 </button>
+//                             </div>
+//                             <div class='postButtonContainer brown'>
+//                                 <button class='likeButton ${likeButtonActiveClass}'>
+//                                     <i class='far fa-heart'></i>
+//                                     <span>${postData.likes.length || ""}</span>
+//                                 </button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>`;
+// }
+
+// function timeDifference(current, previous) {
+
+//     var msPerMinute = 60 * 1000;
+//     var msPerHour = msPerMinute * 60;
+//     var msPerDay = msPerHour * 24;
+//     var msPerMonth = msPerDay * 30;
+//     var msPerYear = msPerDay * 365;
+
+//     var elapsed = current - previous;
+
+//     if (elapsed < msPerMinute) {
+//         if(elapsed/1000 < 30) return "Just now";
+
+//         return Math.round(elapsed/1000) + ' seconds ago';
+//     }
+
+//     else if (elapsed < msPerHour) {
+//          return Math.round(elapsed/msPerMinute) + ' minutes ago';
+//     }
+
+//     else if (elapsed < msPerDay ) {
+//          return Math.round(elapsed/msPerHour ) + ' hours ago';
+//     }
+
+//     else if (elapsed < msPerMonth) {
+//         return Math.round(elapsed/msPerDay) + ' days ago';
+//     }
+
+//     else if (elapsed < msPerYear) {
+//         return Math.round(elapsed/msPerMonth) + ' months ago';
+//     }
+
+//     else {
+//         return Math.round(elapsed/msPerYear ) + ' years ago';
+//     }
+// }
+$("#postTextarea, #replyTextarea").keyup(event => {
     var textbox = $(event.target);
-    // console.log(textbox);
     var value = textbox.val().trim();
-    // console.log(value);
 
-    var submitButton = $("#submitPostButton");
+    var isModal = textbox.parents(".modal").length == 1;
+    
+    var submitButton = isModal ? $("#submitReplyButton") : $("#submitPostButton");
 
-    if(submitButton.length == 0) return alert("No submit button found!");
+    if(submitButton.length == 0) return alert("No submit button found");
 
-    if(value == ""){
+    if (value == "") {
         submitButton.prop("disabled", true);
         return;
     }
 
     submitButton.prop("disabled", false);
 })
+
 $("#submitPostButton").click(() => {
     var button = $(event.target);
-    var textbox =$("#postTextarea");
+    var textbox = $("#postTextarea");
 
     var data = {
-        content: textbox.val(),
+        content: textbox.val()
     }
 
     $.post("/api/posts", data, postData => {
-
+        
         var html = createPostHtml(postData);
         $(".postsContainer").prepend(html);
         textbox.val("");
@@ -37,24 +245,35 @@ $("#submitPostButton").click(() => {
     })
 })
 
+$("#replyModal").on("show.bs.modal", (event) => {
+    var button = $(event.relatedTarget);
+    var postId = getPostIdFromElement(button);
+
+    $.get("/api/posts/" + postId, results => {
+        outputPosts(results, $("#originalPostContainer"))
+    })
+})
+
 $(document).on("click", ".likeButton", (event) => {
     var button = $(event.target);
     var postId = getPostIdFromElement(button);
-
+    
     if(postId === undefined) return;
 
     $.ajax({
         url: `/api/posts/${postId}/like`,
         type: "PUT",
         success: (postData) => {
+            
             button.find("span").text(postData.likes.length || "");
 
-            if(postData.likes.includes(userLoggedIn._id)){
+            if(postData.likes.includes(userLoggedIn._id)) {
                 button.addClass("active");
             }
-            else{
+            else {
                 button.removeClass("active");
             }
+
         }
     })
 
@@ -89,7 +308,7 @@ function getPostIdFromElement(element) {
     var rootElement = isRoot == true ? element : element.closest(".post");
     var postId = rootElement.data().id;
 
-    if(postId === undefined) return alert("Post Id undefined");
+    if(postId === undefined) return alert("Post id undefined");
 
     return postId;
 }
@@ -101,8 +320,6 @@ function createPostHtml(postData) {
     var isRetweet = postData.retweetData !== undefined;
     var retweetedBy = isRetweet ? postData.postedBy.username : null;
     postData = isRetweet ? postData.retweetData : postData;
-
-    console.log(isRetweet);
     
     var postedBy = postData.postedBy;
 
@@ -112,6 +329,7 @@ function createPostHtml(postData) {
 
     var displayName = postedBy.firstName + " " + postedBy.lastName;
     var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
+
     var likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? "active" : "";
     var retweetButtonActiveClass = postData.retweetUsers.includes(userLoggedIn._id) ? "active" : "";
 
@@ -142,7 +360,7 @@ function createPostHtml(postData) {
                         </div>
                         <div class='postFooter'>
                             <div class='postButtonContainer'>
-                                <button>
+                                <button data-toggle='modal' data-target='#replyModal'>
                                     <i class='far fa-comment'></i>
                                 </button>
                             </div>
@@ -152,7 +370,7 @@ function createPostHtml(postData) {
                                     <span>${postData.retweetUsers.length || ""}</span>
                                 </button>
                             </div>
-                            <div class='postButtonContainer brown'>
+                            <div class='postButtonContainer red'>
                                 <button class='likeButton ${likeButtonActiveClass}'>
                                     <i class='far fa-heart'></i>
                                     <span>${postData.likes.length || ""}</span>
@@ -176,27 +394,44 @@ function timeDifference(current, previous) {
 
     if (elapsed < msPerMinute) {
         if(elapsed/1000 < 30) return "Just now";
-
-        return Math.round(elapsed/1000) + ' seconds ago';
+        
+        return Math.round(elapsed/1000) + ' seconds ago';   
     }
 
     else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + ' minutes ago';
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
     }
 
     else if (elapsed < msPerDay ) {
-         return Math.round(elapsed/msPerHour ) + ' hours ago';
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
     }
 
     else if (elapsed < msPerMonth) {
-        return Math.round(elapsed/msPerDay) + ' days ago';
+        return Math.round(elapsed/msPerDay) + ' days ago';   
     }
 
     else if (elapsed < msPerYear) {
-        return Math.round(elapsed/msPerMonth) + ' months ago';
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
     }
 
     else {
-        return Math.round(elapsed/msPerYear ) + ' years ago';
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
     }
+}
+
+function outputPosts(results, container){
+    container.html("");
+    
+    if(!Array.isArray(results)){
+        results = [results];
+    }
+    results.forEach(result => {
+        var html = createPostHtml(result);
+        container.append(html);
+    });
+
+    if(results.length == 0){
+        container.append("<span class = 'noResults'>Nothing to Show. </span>")
+    }
+
 }
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,3 +1,138 @@
+// const express = require('express');
+// const app = express();
+// const router = express.Router();
+// const bodyParser = require("body-parser")
+// const User = require('../../schemas/UserSchema');
+// const Post = require('../../schemas/PostSchema');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// router.get("/", (req, res, next) => {
+//     Post.find()
+//     .populate("postedBy")
+//     .populate("retweetData")
+//     .sort({ "createdAt": -1 })
+//     .then(async results => {
+//         results = await User.populate(results, { path: "retweetData.postedBy"});
+//         res.status(200).send(results);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+// })
+
+// router.get("/:id", (req, res, next) => {
+//     return res.status(200).send("This is awesome")
+//     Post.find()
+//     .populate("postedBy")
+//     .populate("retweetData")
+//     .sort({ "createdAt": -1 })
+//     .then(async results => {
+//         results = await User.populate(results, { path: "retweetData.postedBy"});
+//         res.status(200).send(results);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+// })
+
+
+// router.post("/", async (req, res, next) => {
+
+//     if (!req.body.content) {
+//         console.log("Content param not sent with request");
+//         return res.sendStatus(400);
+//     }
+
+//     var postData = {
+//         content: req.body.content,
+//         postedBy: req.session.user
+//     }
+
+//     Post.create(postData)
+//     .then(async newPost => {
+//         newPost = await User.populate(newPost, { path: "postedBy" })
+
+//         res.status(201).send(newPost);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+// })
+
+// router.put("/:id/like", async (req, res, next) => {
+
+//     var postId = req.params.id;
+//     var userId = req.session.user._id;
+
+//     var isLiked = req.session.user.likes && req.session.user.likes.includes(postId);
+
+//     var option = isLiked ? "$pull" : "$addToSet";
+
+
+//     req.session.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId } }, { new: true})
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+
+
+//     var post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId } }, { new: true})
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+
+
+//     res.status(200).send(post)
+// })
+
+// router.post("/:id/retweet", async (req, res, next) => {
+//     var postId = req.params.id;
+//     var userId = req.session.user._id;
+
+//     // Try and delete retweet
+//     var deletedPost = await Post.findOneAndDelete({ postedBy: userId, retweetData: postId })
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+
+//     var option = deletedPost != null ? "$pull" : "$addToSet";
+
+//     var repost = deletedPost;
+
+//     if (repost == null) {
+//         repost = await Post.create({ postedBy: userId, retweetData: postId })
+//         .catch(error => {
+//             console.log(error);
+//             res.sendStatus(400);
+//         })
+//     }
+
+//     // Insert user like
+//     req.session.user = await User.findByIdAndUpdate(userId, { [option]: { retweets: repost._id } }, { new: true })
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+
+//     // Insert post like
+//     var post = await Post.findByIdAndUpdate(postId, { [option]: { retweetUsers: userId } }, { new: true })
+//     .catch(error => {
+//         console.log(error);
+//         res.sendStatus(400);
+//     })
+
+
+//     res.status(200).send(post)
+// })
+
+// module.exports = router;
+
 const express = require('express');
 const app = express();
 const router = express.Router();
@@ -7,19 +142,44 @@ const Post = require('../../schemas/PostSchema');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {
-    Post.find()
-    .populate("postedBy")
-    .populate("retweetData")
-    .sort({ "createdAt": -1 })
-    .then(async results => {
-        results = await User.populate(results, { path: "retweetData.postedBy"});
-        res.status(200).send(results);
-    })
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
+router.get("/", async (req, res, next) => {
+    // Post.find()
+    // .populate("postedBy")
+    // .populate("retweetData")
+    // .sort({ "createdAt": -1 })
+    // .then(async results => {
+    //     results = await User.populate(results, { path: "retweetData.postedBy"});
+    //     res.status(200).send(results);
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    //     res.sendStatus(400);
+    // })
+    var results = await getPosts({})
+    res.status(200).send(results);
+})
+
+router.get("/:id", async (req, res, next) => {
+
+    var postId = req.params.id;
+
+    var results = await getPosts({_id : postId});
+    results = results[0];
+    console.log(results);
+    return res.status(200).send(results);
+
+    // Post.find()
+    // .populate("postedBy")
+    // .populate("retweetData")
+    // .sort({ "createdAt": -1 })
+    // .then(async results => {
+    //     results = await User.populate(results, { path: "retweetData.postedBy"});
+    //     res.status(200).send(results);
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    //     res.sendStatus(400);
+    // })
 })
 
 router.post("/", async (req, res, next) => {
@@ -55,14 +215,14 @@ router.put("/:id/like", async (req, res, next) => {
 
     var option = isLiked ? "$pull" : "$addToSet";
 
-
+    // Insert user like
     req.session.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId } }, { new: true})
     .catch(error => {
         console.log(error);
         res.sendStatus(400);
     })
 
-
+    // Insert post like
     var post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId } }, { new: true})
     .catch(error => {
         console.log(error);
@@ -114,4 +274,14 @@ router.post("/:id/retweet", async (req, res, next) => {
     res.status(200).send(post)
 })
 
+async function getPosts(filter){
+    var results = await Post.find(filter)
+    .populate("postedBy")
+    .populate("retweetData")
+    .sort({ "createdAt": -1 })
+    .catch(error => console.log(error))
+
+    return await User.populate(results, { path: "retweetData.postedBy"});
+
+}
 module.exports = router;
